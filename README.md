f5b82c55960a2b5697d13269e44a42330ea0e27f
Sumit Patil <sgpatil.2803@gmail.com>
2015-06-16 16:38:08 +0530

    Updated

---
+++ README.md
@@ -0,0 +1,90 @@
+
+# Orientdb Driver for Laravel 5
+
+
+Orientdb Graph Eloquent Driver for Laravel 5
+
+## Quick Reference
+
+ - [Installation](#installation)
+ - [Configuration](#Database Configuration)
+ - [Migration](#migration)
+
+## Installation
+
+Add the package to your `composer.json` and run `composer update`.
+
+```json
+{
+    "require": {
+        "sgpatil/orientdb-laravel-5": "dev-master"
+    }
+}
+```
+
+Add the service provider in `config/app.php`:
+
+```php
+Sgpatil\Orientdb\OrientdbServiceProvider::class,
+```
+
+This will register all the required classes for this package.
+
+## Database Configuration
+
+Open `config/database.php`
+make `orientdb` your default connection:
+
+```php
+'default' => 'orientdb',
+```
+
+Add the connection defaults:
+
+```php
+'connections' => [
+    'orientdb' => [
+        'driver' => 'orientdb',
+        'host'   => 'localhost',
+        'port'   => '2480',
+        'database' => 'database_name',
+        'username' => 'root',
+        'password' => 'root'
+    ]
+]
+```
+
+Add your database username and password in 'username' and 'password' field. In 'database_name' add name of orientdb database which you want to connect and use.
+
+## Migration
+
+To create a migration, you may use the orient command on the Artisan CLI:
+
+```php
+php artisan orient:make create_users_table
+```
+
+The migration will be placed in your database/migrations folder, and will contain a timestamp which allows the framework to determine the order of the migrations.
+
+The --table and --create options may also be used to indicate the name of the table, and whether the migration will be creating a new table:
+```php
+php artisan orient:make add_votes_to_users_table --table=users_votes
+
+php artisan orient:make create_users_table --create=users
+```
+To run migration 
+```php
+php artisan orient
+```
+
+## How to Use
+```php
+class User extends Orientdb {
+
+    protected $fillable = ['name', 'email'];
+}
+
+$user = User::create(['name' => 'Some Name', 'email' => 'some@email.com']);
+
+```
+You can use this by extending Orientdb into model class. 
---
+++ src/Sgpatil/Orientdb/OrientdbServiceProvider.php
@@ -0,0 +1,76 @@
+<?php namespace Sgpatil\Orientdb;
+
+use Illuminate\Support\ServiceProvider;
+use Sgpatil\Orientdb\Connection;
+use Sgpatil\Orientdb\Migrations\DatabaseMigrationRepository;
+
+class OrientdbServiceProvider extends ServiceProvider {
+
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		//
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+
+               $this->app->register('Sgpatil\Orientdb\MigrationServiceProvider');
+
+        $this->app['db']->extend('orientdb', function($config) {
+            return new Connection($config);
+        });
+
+        $this->app->booting(function() {
+            $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+            $loader->alias('Orientdb', 'Sgpatil\Orientdb\Eloquent\Model');
+        });
+        
+
+        $this->app->bind('ConnectionResolverInterface', function($app) {
+            $conn = new Connection(['host' => 'localhost',
+                'port' => 2480,
+                'username' => 'root',
+                'database' => 'graphdb2',
+                'password' => 'root']);
+            return new DatabaseManager($app, $conn );
+        });
+
+        $this->app->bind('MigrationRepositoryInterface', function($app) {
+            $resolver = $this->app->make('ConnectionResolverInterface');
+            $table = $app['config']['database.migrations'];
+            return new DatabaseMigrationRepository($resolver, $table);
+        });
+        
+        
+
+        $this->app->bind('orientdb.database', function() {
+            return new Connection(['host' => 'localhost',
+                'port' => 2480,
+                'username' => 'root',
+                'database' => 'graphdb2',
+                'password' => 'root']);
+        });
+
+        $this->app->bind('CreateOrientdbMigration', function($app) {
+            //print_r($app);
+            $database = $this->app->make('orientdb.database');
+            return new CreateOrientdbMigration($database);
+        });
+
+        $CreateOrientdbMigration = $this->app->make('CreateOrientdbMigration');
+
+        $this->commands('CreateOrientdbMigration');
+		
+	}
+
+}
