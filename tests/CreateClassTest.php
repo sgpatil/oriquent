feac76fa3889741c162c7fae8dab16dbf1afa519
Sumit Patil <sum_it_patil@yahoo.com>
2016-02-01 12:03:25 +0530

    Testcase - Createclass added

--- tests/CreateClassTest.php
+++ tests/CreateClassTest.php
@@ -1,5 +1,7 @@
 <?php
 namespace Sgpatil\Orientdb;
+use Sgpatil\Orientdb\Schema\Builder;
+use Sgpatil\Orientdb\Schema\Blueprint;
 
 /**
  * Description of CreateClassTest
@@ -7,6 +9,18 @@ namespace Sgpatil\Orientdb;
  * @author sumit
  */
 class CreateClassTest extends BaseTest {
-    
+    public function __construct() {
+        parent::__construct();
+        
+    }
+    public function testCreateClass() {
+        $connection = $this->getConnection('orientdb'); 
+        $query = new Builder($connection);
+        $query->create('testusers', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->timestamps();
+		});
+    }
     
 }
